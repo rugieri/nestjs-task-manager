aee7e966c5441e868e9549b7d8ec89f56a2267f9
rugieri <rugieri.ronaldo@gmail.com>
2021-07-03 21:56:06 -0300

    created a tasks module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TasksModule],
 })
 export class AppModule {}
