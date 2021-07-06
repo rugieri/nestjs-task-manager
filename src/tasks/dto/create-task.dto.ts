05893a511a8413a292218d40fab3ee8d1edb4fe9
rugieri <rugieri.ronaldo@gmail.com>
2021-07-05 23:31:23 -0300

    added data transfer objects CreateTaskDto

---
+++ src/tasks/dto/create-task.dto.ts
@@ -0,0 +1,5 @@
+export class CreateTaskDto {
+  title: string;
+  description: string;
+  
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -9,7 +10,8 @@ export class TasksService {
   getAllTasks(): Task[] {
     return this.tasks;
   }
-  createTask(title: string, description: string): Task {
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
     const task: Task = {
       id: uuid(),
       title,
