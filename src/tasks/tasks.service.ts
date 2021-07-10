6f8ac46ecaa14bef3dc516715e61b1d72b0b97ca
rugieri <rugieri.ronaldo@gmail.com>
2021-07-10 10:08:46 -0300

    added an error handling when getting a non-existing task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -35,7 +35,11 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const found =  this.tasks.find((task) => task.id === id);
+    if (!found) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+    return found;
   }
   createTask(createTaskDto: CreateTaskDto): Task {
     const { title, description } = createTaskDto;
