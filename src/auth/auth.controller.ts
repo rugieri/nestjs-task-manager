2e21a7be584b20b6192de207390f09fc82ca1adb
rugieri <rugieri.ronaldo@gmail.com>
2021-08-03 23:59:36 -0300

    added signin feature

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,4 +9,8 @@ export class AuthController {
   signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.authService.signUp(authCredentialsDto);
   }
+  @Post('/signin')
+  signIn(@Body() authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    return this.authService.signIn(authCredentialsDto);
+  }
 }
