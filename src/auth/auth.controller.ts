76043dd3da7d34ec852ca05133d013d8db00b868
rugieri <rugieri.ronaldo@gmail.com>
2021-08-04 23:53:38 -0300

    added signing a JWT token on sign in (authentication)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,7 +10,9 @@ export class AuthController {
     return this.authService.signUp(authCredentialsDto);
   }
   @Post('/signin')
-  signIn(@Body() authCredentialsDto: AuthCredentialsDto): Promise<string> {
+  signIn(
+    @Body() authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto);
   }
 }
