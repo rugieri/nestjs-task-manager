37fdfb2de208de9dbaa9385b476f4f4e94d97dbe
rugieri <rugieri.ronaldo@gmail.com>
2021-08-07 19:21:00 -0300

    added database relation with tasks and users

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,13 +1,20 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/auth/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
 @Entity()
-export class Task{
+export class Task {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
   @Column()
   title: string;
+
   @Column()
   description: string;
+
   @Column()
   status: TaskStatus;
+
+  @ManyToOne((_type) => User, (user) => user.tasks, { eager: false })
+  user: User;
 }
