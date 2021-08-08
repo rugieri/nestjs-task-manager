49bf7ca052a4d2a2a3a992334ec7493ad0652d8f
rugieri <rugieri.ronaldo@gmail.com>
2021-08-08 02:35:31 -0300

    restricted getting a task by ID

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -33,8 +33,8 @@ export class TasksController {
   }
 
   @Get('/:id')
-  getTaskById(@Param('id') id: string): Promise<Task> {
-    return this.tasksService.getTaskById(id);
+  getTaskById(@Param('id') id: string, @GetUser() user: User): Promise<Task> {
+    return this.tasksService.getTaskById(id, user);
   }
 
   @Post()
