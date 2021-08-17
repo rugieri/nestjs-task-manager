2c5982b591eaaf51d27e54d4e3e01105537ba4dd
rugieri <rugieri.ronaldo@gmail.com>
2021-08-16 23:42:18 -0300

    jwt secret configuration

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,19 +1,20 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayload } from './jwt-payload.interface';
 import { User } from './user.entity';
 import { UsersRepository } from './users.repository';
-
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     @InjectRepository(UsersRepository)
     private usersRepository: UsersRepository,
+    private configService: ConfigService,
   ) {
     super({
-      secretOrKey: 'topSecret51',
+      secretOrKey: configService.get('JWT_SECRET'),
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     });
   }
--- src/config.schema.ts
+++ src/config.schema.ts
@@ -1,5 +1,4 @@
 import * as Joi from '@hapi/joi';
-
 export const configValidationSchema = Joi.object({
   STAGE: Joi.string().required(),
   DB_HOST: Joi.string().required(),
@@ -7,4 +6,5 @@ export const configValidationSchema = Joi.object({
   DB_USERNAME: Joi.string().required(),
   DB_PASSWORD: Joi.string().required(),
   DB_DATABASE: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
 });
