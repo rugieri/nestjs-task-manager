c41f56a98fbbb272a4a06c4f0bf310ab71a1c09c
rugieri <rugieri.ronaldo@gmail.com>
2021-08-23 18:26:32 -0300

    changes to the NestJS app for deployment

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,11 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  const port = 3000;
-  await app.listen(3000);
+  const port = process.env.PORT;
+  await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
